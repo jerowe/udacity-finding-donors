6c5d2f1362006de65d22b2c98b1c9d2a82fa1d4a
Jillian Rowe <jillian.e.rowe@gmail.com>
2018-12-31 10:58:13 +0300

    setting up circle

---
+++ finding_donors_flask_app/flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import jsonify, request
+from flask_cors import CORS
+import json
+import functools
+
+app = Flask(__name__)
+CORS(app, allow_headers=['Content-Type', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers',
+                         'Access-Control-Allow-Methods'])
+
+
+@app.after_request
+def apply_caching(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    data = json.loads(request.data.decode('utf-8'))
+    return jsonify(data)
